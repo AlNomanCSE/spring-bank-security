745390f538996522cca020001728dfbb027b868c
AlNomanCSE <abdullahalnomancse@gmail.com>
2024-12-26 19:16:33 -0800

    added routes

---
+++ src/main/java/com/noman/BankBackendApplication/BankBackendApplication.java
@@ -0,0 +1,15 @@
+package com.noman.BankBackendApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class BankBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BankBackendApplication.class, args);
+	}
+}
+
+
---
+++ src/test/java/com/noman/BankBackendApplication/BankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noman.BankBackendApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
