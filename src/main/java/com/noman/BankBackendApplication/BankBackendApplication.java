b50e512d3477eeb0ce066723a3c103dccb7c8f71
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-01-07 03:42:37 -0800

    added password encoder and compromisedpasswordchecker

--- src/main/java/com/noman/BankBackendApplication/BankBackendApplication.java
+++ src/main/java/com/noman/BankBackendApplication/BankBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
+@ComponentScan({"com.noman.BankBackendApplication.config","com.noman.BankBackendApplication.controller"})
 public class BankBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankBackendApplication.class, args);
--- src/main/java/com/noman/BankBackendApplication/config/ProjectSecurityConfig.java
+++ src/main/java/com/noman/BankBackendApplication/config/ProjectSecurityConfig.java
@@ -2,9 +2,17 @@ package com.noman.BankBackendApplication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,11 +23,27 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(crs -> crs.disable())
                 .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "/myCards", "/myBalance","/myLoans").authenticated() //for all those route authentication needed
                 .requestMatchers("/notices", "/contact","/error").permitAll());  //no authentication needed
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+       UserDetails user = User.withUsername("noman").password("{bcrypt}$2a$12$2ckpUhXH9hRcMNKnQqYtsO.ofs7nWcShrGVZXTshaKTWp9wMbHI1C").authorities("read").build();
+       UserDetails admin = User.withUsername("lama").password("{noop}12345n@12").authorities("admin").build();
+       return new InMemoryUserDetailsManager(user, admin);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+      return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
