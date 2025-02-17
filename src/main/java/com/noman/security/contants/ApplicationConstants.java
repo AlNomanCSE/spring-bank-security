e772a3ddbec8e2300e514fc6875633fcb5ee0a48
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-02-17 12:08:55 +0600

    JWT implimentation added

--- src/main/java/com/noman/security/contants/ApplicationConstants.java
+++ src/main/java/com/noman/security/contants/ApplicationConstants.java
@@ -1,6 +1,8 @@
 package com.noman.security.contants;
 
+import io.jsonwebtoken.Jwts;
+
 public final class ApplicationConstants {
     public static final String JWT_SECRET = "JWT_SECRET";
-    public static final String JWT_SECRET_DEFAULT_VALUE ="jaxbhNoman3xpwt8713";
+    public static final String JWT_SECRET_DEFAULT_VALUE = String.valueOf(Jwts.SIG.HS256.key().build());
 }
